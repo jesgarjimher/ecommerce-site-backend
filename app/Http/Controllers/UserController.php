7604a641a13f1cb112614372837c1d726cbd0540
Jesus Garcia <jesgarjimher@gmail.com>
2025-12-26 20:36:59 +0100

    ft: added login to usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,4 +18,16 @@ class UserController extends Controller
         $user->save();
         return $user;
     }
+
+    function login(Request $req) {
+        $user = User::where("email", $req->email)->first();
+        if(!$user || !Hash::check($req->password, $user->password)){
+            return ["error" => "Email or passord invalid"];
+        }
+        return $user;
+    }
+
+
+
+    
 }
