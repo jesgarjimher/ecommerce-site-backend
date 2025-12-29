fb924cdbbc04d3304d6b155315c65ccb16e5125f
Jesus Garcia <jesgarjimher@gmail.com>
2025-12-29 12:37:35 +0100

    ft: product contrller added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,5 @@ Route::get('/user', function (Request $request) {
 
 Route::post("register", [UserController::class, "register"]);
 Route::post("login", [UserController::class, "login"]);
+
+Route::post("addproduct",[ProductController::class, "addProduct"]);
